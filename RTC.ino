03dfa0a36d896b1c66ee0a58c9256b5a7fe6bd42
IanHernandez16 <ianalejandrohc@gmail.com>
2025-12-09 21:10:00 -0500

    RTC codigo

---
+++ RTC.ino
@@ -0,0 +1,32 @@
+#include <RTClib.h>
+
+RTC_DS3231 rtc;
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!rtc.begin()) {
+    Serial.println("RTC no encontrado");
+    while (1);
+  }
+
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
+void loop() {
+  DateTime now = rtc.now();
+
+  Serial.print(now.day());
+  Serial.print("/");
+  Serial.print(now.month());
+  Serial.print("/");
+  Serial.print(now.year());
+  Serial.print("  ");
+  Serial.print(now.hour());
+  Serial.print(":");
+  Serial.print(now.minute());
+  Serial.print(":");
+  Serial.println(now.second());
+
+  delay(1000);
+}
